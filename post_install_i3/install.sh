2ae6004a6abab82bc950f08275029c0d71b8de62
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2024-03-04 18:45:51 +0100

    Update install.sh

--- post_install_i3/install.sh
+++ post_install_i3/install.sh
@@ -54,6 +54,13 @@ sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emula
 sudo update-alternatives --set x-terminal-emulator /usr/local/bin/st
 cd -
 
+# Sublime Text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get install apt-transport-https -y
+sudo apt-get update -y
+sudo apt-get install sublime-text -y
+
 # vim
 sudo apt install -y vim
 cp config/vimrc ~/.vimrc
