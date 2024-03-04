8fb2ed2d211b32e01b36df3506e4cae5621231b5
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2024-03-04 18:49:23 +0100

    Update install.sh

--- post_install_i3/install.sh
+++ post_install_i3/install.sh
@@ -57,7 +57,7 @@ cd -
 # Sublime Text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get install apt-transport-https -y
+sudo apt-get install apt-transport-https lsb-release ca-certificates curl gnupg dirmngr -y
 sudo apt-get update -y
 sudo apt-get install sublime-text -y
 
